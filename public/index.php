b1b9f7a0a53340814610a236c430c20874d4ba9f
jpg-consulting <opensource@jpg-consulting.es>
2017-12-23 15:59:48 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
+use FreelanceInvoice\Mvc\Application;
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
+
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
@@ -17,4 +20,5 @@ if (php_sapi_name() === 'cli-server') {
 // Composer autoloading
 include __DIR__ . '/../vendor/autoload.php';
 
-include __DIR__ . '/../theme/default/authentication.php';
+$Application = new Application();
+$Application->run();
